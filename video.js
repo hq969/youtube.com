f32383de9db56edc2e7cf79cbb1bc65548d6406a
Harsh Sonkar <114162367+hq969@users.noreply.github.com>
2025-03-01 14:04:58 +0530

    Update video.js
    Signed-off-by: Harsh Sonkar <114162367+hq969@users.noreply.github.com>

--- video.js
+++ video.js
@@ -1,39 +1,46 @@
-let video=JSON.parse(localStorage.getItem("videoDetails"))
+let video = JSON.parse(localStorage.getItem("videoDetails"));
 
+document.querySelector("title").textContent = video.title;
 
-document.querySelector("title").textContent=video.title
+let append = () => {
+    let container = document.getElementById("results");
+    container.innerHTML = null;
 
-let append =() => {
+    // Create iframe for video
+    let iframe = document.createElement("iframe");
+    iframe.src = `https://www.youtube.com/embed/${video.id}`;
+    iframe.allowFullscreen = true; // Allow fullscreen mode
+    iframe.id = "videoFrame";
 
-    
-    let container = document.getElementById("results")
+    // Create title
+    let h2 = document.createElement("h2");
+    h2.textContent = video.title;
 
-         container.innerHTML=null
-    
-  
+    // Create fullscreen button
+    let fullscreenButton = document.createElement("button");
+    fullscreenButton.innerText = "Fullscreen";
+    fullscreenButton.classList.add("fullscreen-btn");
 
-        
-        let iframe=document.createElement("iframe")
-        iframe.src = `https://www.youtube.com/embed/${video.id}`
+    // Fullscreen toggle function
+    fullscreenButton.addEventListener("click", function () {
+        let videoFrame = document.getElementById("videoFrame");
+        if (videoFrame.requestFullscreen) {
+            videoFrame.requestFullscreen();
+        } else if (videoFrame.mozRequestFullScreen) { // Firefox
+            videoFrame.mozRequestFullScreen();
+        } else if (videoFrame.webkitRequestFullscreen) { // Chrome, Safari, Opera
+            videoFrame.webkitRequestFullscreen();
+        } else if (videoFrame.msRequestFullscreen) { // IE/Edge
+            videoFrame.msRequestFullscreen();
+        }
+    });
 
-
-        let h2 = document.createElement("h2")
-        h2.textContent=video.title
-
-      
-
-       
-       
-        container.append(iframe,h2)
-
-    
-
-
-
+    container.append(iframe, h2, fullscreenButton);
 };
 
 append();
 
-document.querySelector(".youtubeLogo").addEventListener("click",()=>{
-    window.location.href="index.html"
-})
+// Redirect to homepage on clicking logo
+document.querySelector(".youtubeLogo").addEventListener("click", () => {
+    window.location.href = "index.html";
+});
